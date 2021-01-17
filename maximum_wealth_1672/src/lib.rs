2b25ce76ef47639b63e0c085ba2b00213f58898f
WILLJH <ls_pulse@live.com>
2021-01-18 05:36:58 +0800

    18Jan 5:36

--- maximum_wealth_1672/src/lib.rs
+++ maximum_wealth_1672/src/lib.rs
@@ -22,9 +22,5 @@ mod tests {
 }
 
 pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
-    accounts
-        .iter()
-        .map(|i| i.iter().sum())
-        .max()
-        .unwrap()
+    accounts.iter().map(|i| i.iter().sum()).max().unwrap()
 }
